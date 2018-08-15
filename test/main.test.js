c568fe4c3ee9cdf62b8f216413813230510e1e0f
bonneau <nicolas.bonneau.vlb@gmail.com>
2018-08-15 17:41:49 +0200

    Init tests

---
+++ test/main.test.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+
+var FileLoader = require('../index');
+
+describe('main', function() {
+    it('should make some test', function(done) {
+
+        var fileLoader = FileLoader();
+
+        done();
+    });
+});
